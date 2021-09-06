3cc9f889eb9430c3e186b757e185e3e242888e5e
ShanaGeorge081 <86619347+ShanaGeorge081@users.noreply.github.com>
2021-09-06 14:50:57 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var path,boy
+var pathImg,boyImg
+
+
+
+
+function preload(){
+  //pre-load images
+  pathImg=loadImage("path.png");
+  boyImg=loadAnimation("Runner-1.png","Runner-2.png")
+
+}
+
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+  path=createSprite(200,200);
+  path.addImage(pathImg)
+  path.velocityY=4;
+  path.scale=1.2;
+  
+  boy=createSprite(100,340)
+  boy.addAnimation(boyImg)
+  
+}
+
+
+function draw(){
+
+  background(0);
+  if(path.y>400){
+    path.y=height/2;
+     
+
+  }
+ 
+  drawSprites()
+}
+
+
+
+  
+  
+
+
